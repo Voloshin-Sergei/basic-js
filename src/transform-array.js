460d1ec1d6bbcb472e1fce89d6807b5415888509
Voloshin_Sergey <4voloshin@gmail.com>
2020-10-09 08:41:31 +0300

    init: start task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+if (!Array.isArray(arr) || arr === undefined) {
+  throw new Error();
+};
+const newArr = [];
+
+for (let i  = 0; i < arr.length; i++) {
+  if (arr[i] === '--discard-next' && i + 1 < arr.length) {
+    i++;
+  } else if (arr[i] === '--discard-prev' && i - 1 > 0) {
+    newArr.pop();
+  } else if (arr[1] === '--double-next' && i + 1 < arr.length) {
+      newArr.push(arr[i + 1]);
+  } else if (arr[i] === '--double-prev' && i - 1 > 0) {
+    newArr.push(arr[i - 1]);
+  } else {
+      newArr.push(arr[i]);
+  }
+}
+const result = [];
+for (let j = 0; j < newArr.length; j++) {
+  if (typeof newArr[j] == 'number') {
+    result.push(newArr[j]);
+  }
+}
+return result;
 };
