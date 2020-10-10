8e1de3c84c43625ff67a97fc86017bc2350357ca
Voloshin_Sergey <4voloshin@gmail.com>
2020-10-10 13:27:11 +0300

    feat: add solution for transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,29 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-if (!Array.isArray(arr) || arr === undefined) {
+if (!Array.isArray(arr)) {
   throw new Error();
 };
 const newArr = [];
-
 for (let i  = 0; i < arr.length; i++) {
-  if (arr[i] === '--discard-next' && i + 1 < arr.length) {
+  if (arr[i] === '--discard-next') {
+    if (i === arr.length - 1) continue;
     i++;
-  } else if (arr[i] === '--discard-prev' && i - 1 > 0) {
+  } else if (arr[i] === '--discard-prev') {
+    if (i === 0) continue;
+    if (arr[i - 2] === '--discard-next') continue;
     newArr.pop();
-  } else if (arr[1] === '--double-next' && i + 1 < arr.length) {
-      newArr.push(arr[i + 1]);
-  } else if (arr[i] === '--double-prev' && i - 1 > 0) {
+  } else if (arr[i] === '--double-next') {
+    if (i === arr.length - 1) continue;
+    newArr.push(arr[i + 1]);
+  } else if (arr[i] === '--double-prev') {
+    if (i === 0) continue;
+    if (arr[i - 2] === '--discard-next') continue;
     newArr.push(arr[i - 1]);
   } else {
-      newArr.push(arr[i]);
+    newArr.push(arr[i]);
   }
 }
-const result = [];
-for (let j = 0; j < newArr.length; j++) {
-  if (typeof newArr[j] == 'number') {
-    result.push(newArr[j]);
-  }
-}
-return result;
+return newArr;
 };
